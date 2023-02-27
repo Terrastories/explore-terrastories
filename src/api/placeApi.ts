bee6e7aa127b874207f6af6d64e18afa9ae242a6
Laura Mosher <lauramosher@users.noreply.github.com>
2023-02-27 14:04:53 -0500

    Add Place api to get a Place from backend

---
+++ src/api/placeApi.ts
@@ -0,0 +1,9 @@
+import http from './axios'
+
+import type { APIResponse } from './types'
+
+export function getPlace(slug: string, placeId = {}): Promise<APIResponse> {
+  return http.get(`/communities/:slug/places/:placeId`, {
+    embeddedParams: {slug, placeId}
+  })
+}
