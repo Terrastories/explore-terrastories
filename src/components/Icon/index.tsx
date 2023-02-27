ab4d2ee2f9357f5c6a6f97d7c3eb50138e09b63b
Laura Mosher <lauramosher@users.noreply.github.com>
2023-02-27 14:04:52 -0500

    Add Close and Speaker to Icon component

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -3,6 +3,8 @@ import PinIcon from './assets/pin.svg'
 import GridIcon from './assets/gridView.svg'
 import ListIcon from './assets/listView.svg'
 import SortIcon from './assets/sort.svg'
+import CloseIcon from './assets/closeX.svg'
+import SpeakerIcon from './assets/speakerPhone.svg'
 
 export default function Icon({icon, alt}:{icon: string, alt: string}) {
   var src
@@ -21,6 +23,12 @@ export default function Icon({icon, alt}:{icon: string, alt: string}) {
       break
     case 'sort':
       src = SortIcon
+      break
+    case 'close':
+      src = CloseIcon
+      break
+    case 'speaker':
+      src = SpeakerIcon
       break
     default:
       throw new Error('Icon does not exist')
