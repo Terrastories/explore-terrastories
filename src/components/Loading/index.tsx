b4a6fcdf33f2ddf2a8403320e1f296ab97b2eb05
Laura Mosher <lauramosher@users.noreply.github.com>
2023-02-27 14:04:51 -0500

    Convert Loading spinner to standard styles for scaling capabilities

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,52 +1,13 @@
-import styled, { keyframes } from 'styled-components';
-
-const rotate = keyframes`
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-`;
-
-const Ring = styled.div`
-  display: inline-block;
-  position: relative;
-  width: 80px;
-  height: 80px;
-`;
-
-const RingLoader = styled.div`
-  box-sizing: border-box;
-  display: block;
-  position: absolute;
-  width: 64px;
-  height: 64px;
-  margin: 8px;
-  border: 8px solid #000;
-  border-radius: 50%;
-  animation: ${rotate} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: #000 transparent transparent transparent;
-
-  div:nth-child(1) {
-    animation-delay: -0.45s;
-  }
-  div:nth-child(2) {
-    animation-delay: -0.3s;
-  }
-  div:nth-child(3) {
-    animation-delay: -0.15s;
-  }
-`;
+import './styles.css'
 
 export default function Loading() {
   return (
-    <Ring>
-      <RingLoader>
+    <div className={'ringWrapper'}>
+      <div className={'ring'}>
         <div></div>
         <div></div>
         <div></div>
-      </RingLoader>
-    </Ring>
+      </div>
+    </div>
   )
 }
