6216aa79c56ab66dfaa67636d0aa6b51a908491f
Laura Mosher <lauramosher@users.noreply.github.com>
2023-08-05 10:40:25 -0400

    fix: story title overflowing sidebar
    Long story titles without breaks were overflowing sidebar
    
    Fixed using:
    
    1. Clamp to 1 line and add ellipsis to indicate it's truncated.
    2. Added title attr hover as a stopgap to a more robust hover.
    3. Moves language and media indicators to StoryMeta to allow header to
       use a full line to optimize display.

--- src/pages/community/components/SidePanel/components/Story.tsx
+++ src/pages/community/components/SidePanel/components/Story.tsx
@@ -21,29 +21,32 @@ box-shadow: 0 1px 4px rgb(102 102 102 / 10%);
   cursor: pointer;
   box-shadow: 0 0 4px 2px rgb(102 102 102 / 20%);
 }
-`
 
-const StoryHeader = styled.div`
-display: flex;
-justify-content: space-between;
-margin-bottom: 0.5rem;
 h2 {
   font-size: 1.25rem;
   margin: 0;
+  word-break: break-word;
+}
+`
 
-  span {
-    display: flex;
-    align-items: center;
-    font-size: 0.8rem;
+const StoryMeta = styled.div`
+display: flex;
+justify-content: space-between;
+margin-bottom: 0.5rem;
 
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-      sans-serif;
+span {
+  display: flex;
+  align-items: center;
+  font-size: 0.8rem;
 
-    svg {
-      height: 18px;
-      width: 18px;
-    }
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+
+  svg {
+    flex-shrink: 0;
+    height: 18px;
+    width: 18px;
   }
 }
 `
@@ -80,21 +83,21 @@ export default function Story({story}: Props) {
 
   return (
     <StoryListItem onClick={handleStoryClick} data-story-id={story.id}>
-      <StoryHeader>
-        <h2>
-          <span>
-            <Icon icon={'language'} alt={'language'} />
-            {story.language}
-          </span>
-          {story.title}
-        </h2>
+      <StoryMeta>
+        <span>
+          <Icon icon={'language'} alt={'language'} />
+          {story.language}
+        </span>
         {story.mediaContentTypes &&
           <MediaTypesIndicators>
             {story.mediaContentTypes.map((m) => (
               <Icon key={m} icon={m} alt={m} />
             ))}
           </MediaTypesIndicators>}
-      </StoryHeader>
+      </StoryMeta>
+      <h2 className='clampTitle' title={story.title}>
+        {story.title}
+      </h2>
       <p className={'clamp'}>{story.desc}</p>
     </StoryListItem>
   )
