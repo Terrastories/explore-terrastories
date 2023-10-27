82cb0eb5a094f0c82ee51b0d23eab981f88cff25
Laura Mosher <lauramosher@users.noreply.github.com>
2023-10-27 14:42:17 -0400

    Improve API type interfaces & standarize API

--- src/api/communityApi.ts
+++ src/api/communityApi.ts
@@ -1,14 +1,15 @@
 import http from "./axios"
 
 import type { APIResponse } from "./types"
+import type { TypeCommunity } from "types"
 
-export function getCommunities(search: string | null): Promise<APIResponse> {
+export function getCommunities(search: string | null): Promise<APIResponse<TypeCommunity[]>> {
   const searchParams:{search?: string} = {}
   if (search) { searchParams["search"] = search }
 
   return http.get("/communities", {params: searchParams})
 }
 
-export function getCommunity(slug: string): Promise<APIResponse> {
+export function getCommunity(slug: string): Promise<APIResponse<TypeCommunity>> {
   return http.get("/communities/:slug", {embeddedParams: {slug}})
 }
--- src/api/placeApi.ts
+++ src/api/placeApi.ts
@@ -1,8 +1,9 @@
 import http from "./axios"
 
 import type { APIResponse } from "./types"
+import type { TypePlace } from "types"
 
-export function getPlace(slug: string, placeId = {}): Promise<APIResponse> {
+export function getPlace(slug: string, placeId = {}): Promise<APIResponse<TypePlace>> {
   return http.get("/communities/:slug/places/:placeId", {
     embeddedParams: {slug, placeId}
   })
